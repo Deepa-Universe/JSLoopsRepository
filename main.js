38150ad4b561fa34d199ec8b9073cd11c79640d4
Deepa-Universe <deepa.miracles@gmail.com>
2020-01-19 09:49:16 +0530

    for loop

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict"; 
+//Loops
+
+let arrayObject = [1, 4, 5, "Deepa", "Universe", true, "Miracles", 111];
+
+//Printing the values of the loop using for Loop
+for(let counter = 0; counter = arrayObject.length - 1; counter ++)
+{
+    console.log(counter);
+}
